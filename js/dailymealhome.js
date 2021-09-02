32b6a0496da665e31c2867118155bd636ffcb5e8
Constance Dami <constancedami@hotmail.com>
2021-09-02 19:05:44 +0200

    home navbar con stili

--- js/dailymealhome.js
+++ js/dailymealhome.js
@@ -7,3 +7,31 @@ $(document).ready(function(){
    document.getElementById("maintitle").innerHTML = '<span><img src="imgs/MedievalBackgrounds/T.png" alt=""></span>he Daily Meal';
  }
 });
+function changestyle(name){
+  document.getElementById('whichstylehome').setAttribute("href", "css/"+name);
+
+  if (name=='homemedieval.css'){
+    $("#mainhometext").wrap("<div class='row' id='rowmed'></div>");
+    $("#mainhometext").wrap('<div class="col col-lg-9"></div>');
+    $("#thedailymeal .row").prepend('<div class="col-lg-2 d-none d-lg-block border-medieval"></div>');
+    $("#thedailymeal .row").append('<div class="col-lg-1 d-none d-lg-block border-medieval"></div>');
+    document.getElementById("maintitle").innerHTML = '<span><img src="imgs/MedievalBackgrounds/T.png" alt=""></span>he Daily Meal';
+  }
+  else if (document.getElementById("rowmed")){
+    $(".border-medieval").remove();
+    $("#mainhometext").unwrap();
+    $("#mainhometext").unwrap();
+    document.getElementById("maintitle").innerHTML = 'The Daily Meal';
+  }
+  else{
+    $(".border-medieval").remove();
+    document.getElementById("maintitle").innerHTML = 'The Daily Meal';
+  };
+
+  if (name == 'homepopart.css' || name == 'homebauhaus.css'){
+    document.getElementById('homestylebase').setAttribute("href", "");
+  }
+  else{
+    document.getElementById('homestylebase').setAttribute("href", "./css/home.css");
+  }
+};
