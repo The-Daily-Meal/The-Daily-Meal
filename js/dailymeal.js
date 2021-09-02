599857fcfb3d38f879f1f667a5f84ab07f499c84
Constance Dami <constancedami@hotmail.com>
2021-09-02 13:40:00 +0200

    immerseave

--- js/dailymeal.js
+++ js/dailymeal.js
@@ -4,7 +4,7 @@ function changestyle(name) {
   }
   else{
     $("div.header b:empty").remove();
-  }
+  };
   if (name !="medievalstyle.css"){
 
     $("div").remove(".border-medieval");
@@ -22,8 +22,26 @@ function changestyle(name) {
     $("#ic1-header h2.thing2_authors:last").after('<img src="imgs/MedievalBackgrounds/th.jpg" alt="medieval food image 1">');
     $("#ic2-header h2.thing2_authors:last").after('<img src="imgs/MedievalBackgrounds/th (1).jpg" alt="medieval food image 2">');
     $("#ic3-header h2.thing2_authors:last").after('<img src="imgs/MedievalBackgrounds/1648a031180fd7bb6f201c0534889b88.jpg" alt="medieval food image 3">');
-  }
+  };
+  if (name == "immerseave.css"){
+    $(".content").addClass("d-none");
+    $("body").prepend('<div class="container loadingscreen d-block"><div class="os-phrases" id="os-phrases"><audio autoplay loop src="sounds/mixkit-small-waves-harbor-rocks-1208.wav" id="wavessound"><source src="sounds/mixkit-small-waves-harbor-rocks-1208.wav" type="audio/wav">Your browser does not support the audio element.</audio><h2>Welcome</h2><h2>To The Daily Meal</h2></div></div>');
 
+    $("#os-phrases > h2")
+      .css('opacity', 1).lettering( 'words' )
+      .children( "span" ).lettering()
+      .children( "span" ).lettering();
+     var t = setTimeout(function(){
+        $(".loadingscreen").addClass('d-none');
+        $(".content").removeClass("d-none");
+      }, 12000);
+      $("body").click(function(){
+        clearTimeout(t);
+      })
+  }
+  else{
+    $(".loadingscreen").remove();
+  }
   document.getElementById('whichstyle').setAttribute("href", "css/"+name);
 };
 
@@ -41,6 +59,19 @@ $(document).ready(function(){
   }
   else if ($("#whichstyle[href='css/ourbauhaus.css']")) {
     $("div.header").append('<b class="vertical_bar1"></b><b class="bottom_bar2"></b><b class="top_bar3"></b><b class="circle1"></b><b class="square"></b>');
+  }
+  else if ($("#whichstyle[href='css/immerseave.css']")) {
+    $(".content").addClass("d-none");
+    $("body").prepend('<div class="container loadingscreen d-block"><div class="os-phrases" id="os-phrases"><audio autoplay loop src="sounds/mixkit-small-waves-harbor-rocks-1208.wav" id="wavessound"><source src="sounds/mixkit-small-waves-harbor-rocks-1208.wav" type="audio/wav">Your browser does not support the audio element.</audio><h2>Welcome</h2><h2>To The Daily Meal</h2></div></div>');
+
+    $("#os-phrases > h2")
+      .css('opacity', 1).lettering( 'words' )
+      .children( "span" ).lettering()
+      .children( "span" ).lettering();
+     setTimeout(function(){
+        $(".loadingscreen").addClass('d-none');
+        $(".content").removeClass("d-none");
+      }, 12000);
   }
 
 });
