7e582b02e95ff1f81b2dbb889a8a825053124531
Alice Bordignon <bordignon.alice98@gmail.com>
2021-09-03 18:02:23 +0200

    search

---
+++ js/searchbox.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  $(".panel-heading").click(function(){
+    $(".panel-body").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#arthead-p1").click(function(){
+    $("#artbody-p1").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#arthead-p2").click(function(){
+    $("#artbody-p2").slideToggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#arthead-p3").click(function(){
+    $("#artbody-p3").slideToggle("slow");
+  });
+});
