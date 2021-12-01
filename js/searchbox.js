adf8e984a05262c0c7e8eca01429ff37ccbe179e
GiuliaMenna <72873975+GiuliaMenna@users.noreply.github.com>
2021-12-01 18:13:28 +0100

    pushi pushi

--- js/searchbox.js
+++ js/searchbox.js
@@ -256,4 +256,22 @@ $(document).ready(function(){
           $("span[about="+name+"]").css('background-color', 'transparent');
         }
     });
+    $('input[name="check1"]').click(function(){
+        if($(this).prop("checked") == false){
+          $("#article1").css('opacity', '0.3');
+          $("#article1body").css('opacity', '0.3');
+    };
+  });
+    $('input[name="check2"]').click(function(){
+      if($(this).prop("checked") == false){
+        $("#article2").css('opacity', '0.3');
+        $("#article2body").css('opacity', '0.3');
+  };
+});
+    $('input[name="check3"]').click(function(){
+    if($(this).prop("checked") == false){
+      $("#article3").css('opacity', '0.3');
+      $("#article3body").css('opacity', '0.3');
+};
+});
 });
