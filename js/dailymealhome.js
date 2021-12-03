54a4a004f9556da07c1c14a7315773a97226f100
Constance Dami <constancedami@hotmail.com>
2021-12-03 21:19:24 +0100

    medieval done

--- js/dailymealhome.js
+++ js/dailymealhome.js
@@ -33,7 +33,6 @@ $(document).ready(function(){
    $("#mainhometext").wrap('<div class="col col-lg-9"></div>');
    $("#thedailymeal .row").prepend('<div class="col-lg-2 d-none d-lg-block border-medieval"></div>');
    $("#thedailymeal .row").append('<div class="col-lg-1 d-none d-lg-block border-medieval"></div>');
-   document.getElementById("maintitle").innerHTML = '<span><img src="imgs/MedievalBackgrounds/T.png" alt=""></span>he Daily Meal';
   }
   else if (document.getElementById("whichstylehome").getAttribute("href")=="css/homeimmerseave.css"){
    /*removing the elements before adding them, in case the user clicks on this style, while already displayed*/
@@ -49,10 +48,10 @@ $(document).ready(function(){
    $("body").click(function(){
      document.getElementById('wavessound').play();
    });
-   /*adding a button for the rain functionality*/
-   $("#dropdownMenuButton2").after('<div class="justify-content px-4" id="toremove1"><button  class="btn btn-secondary" type="button" id="makeitrain" rain="no" onclick="makeitrain()"><img src="imgs/immerseave/clipart175629.png" style="width:25px;" id="makeitrainimg"></button></div>');
    /*adding a button to mute the sound*/
    $("#dropdownMenuButton2").after('<div class="justify-content px-4" id="toremove2"><button  class="btn btn-secondary" type="button" id="turnsound" onclick="toggleSound()"><svg id="soundoff" display="none" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-volume-mute-fill" viewBox="0 0 16 16"><path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/></svg><svg id="soundon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-volume-up-fill" viewBox="0 0 16 16"><path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/><path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/><path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"/></svg></button></div>');
+   /*adding a button for the rain functionality*/
+   $("#dropdownMenuButton2").after('<div class="justify-content px-4" id="toremove1"><button  class="btn btn-secondary" type="button" id="makeitrain" rain="no" onclick="makeitrain()"><img src="imgs/immerseave/clipart175629.png" style="width:25px;" id="makeitrainimg"></button></div>');
    /*rain, initialized as not displayed*/
    $("body").prepend('<div class="content rain d-none" id="rain"></div>');
 
@@ -83,17 +82,14 @@ function changestyle(name){
     $("#mainhometext").wrap('<div class="col col-lg-9"></div>');
     $("#thedailymeal .row").prepend('<div class="col-lg-2 d-none d-lg-block border-medieval"></div>');
     $("#thedailymeal .row").append('<div class="col-lg-1 d-none d-lg-block border-medieval"></div>');
-    document.getElementById("maintitle").innerHTML = '<span><img src="imgs/MedievalBackgrounds/T.png" alt=""></span>he Daily Meal';
   }
   else if (document.getElementById("rowmed") && name!='homemedieval.css'){
     $(".border-medieval").remove();
     $("#mainhometext").unwrap();
     $("#mainhometext").unwrap();
-    document.getElementById("maintitle").innerHTML = 'The Daily Meal';
   }
   else if (name != 'homemedieval.css'){
     $(".border-medieval").remove();
-    document.getElementById("maintitle").innerHTML = 'The Daily Meal';
   };
 
   if (name == 'homeimmerseave.css'){
@@ -107,10 +103,10 @@ function changestyle(name){
     /*adding an introductive loading screen, that contains the audio for this style*/
     $(".content").addClass("d-none");
     $("body").prepend('<div class="container loadingscreen d-block" id="loadingscreen"><p style="opacity:0.33;">(Click anywhere to skip this sequence)</p><div class="os-phrases" id="os-phrases"><audio autoplay loop src="sounds/mixkit-small-waves-harbor-rocks-1208.wav" id="wavessound"><source src="sounds/mixkit-small-waves-harbor-rocks-1208.wav" type="audio/wav">Your browser does not support the audio element.</audio><h2>Welcome</h2><h2>To The Daily Meal</h2></div></div>');
-    /*adding a button for the rain functionality*/
-    $("#dropdownMenuButton2").after('<div class="justify-content px-4" id="toremove1"><button  class="btn btn-secondary" type="button" id="makeitrain" rain="no" onclick="makeitrain()"><img src="imgs/immerseave/clipart175629.png" style="width:25px;" id="makeitrainimg"></button></div>');
     /*adding a button to mute the sound*/
     $("#dropdownMenuButton2").after('<div class="justify-content px-4" id="toremove2"><button  class="btn btn-secondary" type="button" id="turnsound" onclick="toggleSound()"><svg id="soundoff" display="none" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-volume-mute-fill" viewBox="0 0 16 16"><path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z"/></svg><svg id="soundon" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-volume-up-fill" viewBox="0 0 16 16"><path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/><path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/><path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"/></svg></button></div>');
+    /*adding a button for the rain functionality*/
+    $("#dropdownMenuButton2").after('<div class="justify-content px-4" id="toremove1"><button  class="btn btn-secondary" type="button" id="makeitrain" rain="no" onclick="makeitrain()"><img src="imgs/immerseave/clipart175629.png" style="width:25px;" id="makeitrainimg"></button></div>');
     /*rain, initialized as not displayed*/
     $("body").prepend('<div class="content rain d-none" id="rain"></div>');
 
@@ -148,8 +144,9 @@ function changestyle(name){
 function makeitrain(){
   /*checking if the rain button is set on rain or not*/
   if(document.getElementById("makeitrain").getAttribute("rain")=="no"){
-    /*set the storm gif as background of the page*/
-    $("#contenthome").css("background-image", "url('imgs/immerseave/storm.gif')");
+    /*set the storm gif as background of the page and change the color of the text*/
+    $("#content").css("background-image", "url('imgs/immerseave/storm.gif')");
+    $("p").css("color", "white");
     /*change the value of the rain button*/
     document.getElementById("makeitrain").setAttribute("rain", "yes");
     /*display the rain*/
@@ -161,8 +158,9 @@ function makeitrain(){
     document.getElementById("wavessound").setAttribute("src", "sounds/mixkit-stormy-sea-waves-loop-1199.wav");
   }
   else{
-    /*change the background gif back to quiet sea*/
-    $("#contenthome").css("background-image", "url('imgs/immerseave/4TF4.gif')");
+    /*change the background gif back to quiet sea and change the color of the text*/
+    $("#content").css("background-image", "url('imgs/immerseave/4TF4.gif')");
+    $("p").css("color", "black");
     /*change the value of the rain button*/
     document.getElementById("makeitrain").setAttribute("rain", "no");
     /*hide the rain*/
