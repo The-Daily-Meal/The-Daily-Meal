21ad1e7cdd916b04990c87cd35036e55cf0a6093
Constance Dami <constancedami@hotmail.com>
2021-12-06 13:00:38 +0100

    med

--- js/dailymeal.js
+++ js/dailymeal.js
@@ -1,5 +1,5 @@
 function keepstyle(whichstyle){
-  var style= document.getElementById(whichstyle).getAttribute("href").replace(/ourcss|our|style|css\/|\.css/g, "");
+  var style= document.getElementById('whichstyle').getAttribute("href").replace(/ourcss|our|style|css\/|\.css/g, "");
   if (style == "cioe"){
     style+="style";
   }
@@ -71,10 +71,16 @@ function changestyle(name) {
     $(".col-lg-9").before("<div class='col-lg-2 d-none d-lg-block border-medieval'></div>");
     $(".col-lg-9").after("<div class='col-lg-1 d-none d-lg-block border-medieval'></div>");
 
+    /*$("img").each(function(){
+      var ref = $(this).attr("src").replace(".jpg","");
+      $(this).attr("src", ref+"med.jpg");
+    });*/
+
     /*adding medieval images to the headers of articles*/
     $(".content > .container > .row:last .col-4:first-of-type .thing1_title").before('<img src="imgs/MedievalBackgrounds/th.jpg" style="border-radius: 40% 40% 0% 0%;" alt="medieval food image 1" id="medimg1">');
     $(".content > .container > .row:last .col-4:nth-of-type(2) .thing1_title").before('<img src="imgs/MedievalBackgrounds/th (1).jpg" style="border-radius: 40% 40% 0% 0%;" alt="medieval food image 2" id="medimg2">');
     $(".content > .container > .row:last .col-4:nth-of-type(3) .thing1_title").before('<img src="imgs/MedievalBackgrounds/1648a031180fd7bb6f201c0534889b88.jpg" style="border-radius: 40% 40% 0% 0%;" alt="medieval food image 3" id="medimg3">');
+
   }
   /*otherwise remove decorations linked to medieval style only*/
   else{
